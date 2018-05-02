409ff48495b73a3775ffecb44184b0af2ab4aa8c
MrLalatg <MrLalatg@gmail.com>
2018-05-02 19:45:04 +0300

    gamma :D

--- src/main.java
+++ src/main.java
@@ -15,11 +15,14 @@ import lejos.nxt.Motor;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.comm.RConsole;
+import lejos.util.Delay;
+import lejos.util.Stopwatch;
 
 class writer {
-	void fw(indi obj) {
+	void fw(indi obj, String filename) {
 		FileOutputStream fos = null;
-		File inform = new File("inf.txt");
+		File inform = new File(filename + ".txt");
 		
 	    try {
 	        fos = new FileOutputStream(inform, true);
@@ -36,7 +39,8 @@ class writer {
 	    	dOut.writeChars("KD:" + obj.coef.get("kd").toString() + "\n");
 	    	dOut.writeChars("KI:" + obj.coef.get("ki").toString() + "\n");
 	    	dOut.writeChars("C:" + obj.coef.get("c").toString() + "\n");
-	    	dOut.writeChars("ID:" + obj.id + "\n");
+	    	dOut.writeChars("REASON:" + obj.reason + "\n");
+	    	dOut.writeChars("ID:" +obj.id + "\n");
 	    	dOut.writeChars("POKOLENIE:" + obj.pok + "\n");
 	    	fos.close();
 	    } catch(IOException ex) {
@@ -56,12 +60,12 @@ class indi{
 	@SuppressWarnings("deprecation")
 	Map<String, Double> coef = new HashMap<String, Double>();
 	
-	double time;
+	int time;
 	boolean isParent = false;
 	String reason;
 	int cross = 0;
 	int ePr = 0;
-	boolean flag = false;
+	public boolean flag = false;
 	int pok;
 	int id = 0;
 	static int cnt = 0;
@@ -79,39 +83,61 @@ class indi{
 		Motor.B.suspendRegulation();
 		RMotor.setPower((int) pwrR);
 		LMotor.setPower((int) pwrL);
-		if(RLight.readValue()<50 && LLight.readValue()<50) {
+		if(RLight.readValue()<35 && LLight.readValue()<35) {
 			 cross+=1;
 		}
+		
+		switch(cross) {
+		case 1:
+			RMotor.setPower(50);
+			LMotor.setPower(50);
+			RMotor.forward();
+			LMotor.forward();
+			Delay.msDelay(500);
+		
+		}
 	}
-	
+	public void message(String msg) {
+		FileOutputStream fs = null;
+		File log = new File("mylog.txt");
+		
+		try {
+			fs = new FileOutputStream(log, true);
+			DataOutputStream out = new DataOutputStream(fs);
+			out.writeChars(id + " " + pok + " " + cross + " " + msg + "\n");
+			out.close();
+		} catch(IOException ex) {
+			
+		}
+	}
 	
 	public void run() {
+		message("run start");
+		Stopwatch sw = new Stopwatch();
+		reason = "Success";
 		while(cross<2) {
+			message("while bf pid()");
 			pid();
-			if(flag) {
+			message("while af pid()");
+			if(Button.ENTER.isPressed()) {
+				message("Pass into button press");
 				reason = "Fail";
+				break;
 			}
 		}
-		reason = "Success";
+		time = sw.elapsed();
 		RMotor.stop();
 		LMotor.stop();
+		message("Exitting run()");
+		return;
 	}
 	public indi(double op, double od, double oi, double oc, int opok) {
-		Button.ENTER.addButtonListener(new ButtonListener() {
-			public void buttonPressed(Button b){
-				LCD.drawString("PROGRAM INTERRUPTED", 31, 3);
-			}
-			
-			public void buttonReleased(Button b) {
-				LCD.clear();
-				flag = true;
-			}
-		});
 		id = cnt;
 		coef.put("kp", op);
 		coef.put("kd", od);
 		coef.put("ki", oi);
 		coef.put("c", oc);
+		pok = opok;
 		cnt++;
 	}
 }
@@ -119,20 +145,21 @@ class indi{
 class experiment{
 	String[] coefNames = {"kp", "kd", "ki", "c"};
 	double maxtime;
+	String fPath;
 	ArrayList<indi> curGen = new ArrayList<>();
 	
-	experiment(ArrayList<indi> ar) {
+	experiment(ArrayList<indi> ar, String path) {
 		curGen = ar;
+		fPath = path;
 	}		  	 
 	ArrayList<indi> selection() {
 		ArrayList<indi> tempGen = new ArrayList<>();
-		for(int i = 0; i<= curGen.size(); i++) {
+		for(int i = 0; i < curGen.size(); i++) {
 			curGen.get(i).run();
 			if(isAlive(curGen.get(i))) {
 				tempGen.add(curGen.get(i));
 			}
 		}
-		
 		return tempGen;
 	}
 	
@@ -167,18 +194,20 @@ class experiment{
 				result.coef.put(tcoef, i2.coef.get(tcoef)); 
 			}
 		}
-		
+		result.pok++;
 		return result;
 	}
 	
 	void run() {
 		writer w = new writer();
 		for(int j = 0; j<=9; j++) {
-			for(int i = 0; i<=curGen.size(); i++) {
+			for(int i = 0; i<curGen.size(); i++) {
 				curGen.get(i).run();
 				
-				w.fw(curGen.get(i));
-				LCD.drawString("To run next individual\n press any button", 31, 4);
+				w.fw(curGen.get(i), fPath);
+				LCD.drawString("To run next", 31, 1);
+				LCD.drawString("Individual", 31, 2);
+				LCD.drawString("Press any button", 31, 3);
 				Button.waitForAnyPress();
 			}
 			curGen = selection();
@@ -188,7 +217,7 @@ class experiment{
 	}
 	
 	boolean isAlive(indi ind) {
-		if(ind.time < maxtime && ind.reason == "Success") {
+		if(/*ind.time < maxtime &&*/ ind.reason == "Success") {
 			return true;
 		} else {
 			return false;
@@ -198,23 +227,35 @@ class experiment{
 }
 
 public class main {
-	
+
 	public static void main(String[] args) {
+		Button.ESCAPE.addButtonListener(new ButtonListener() {
+
+			@Override
+			public void buttonPressed(Button b) {
+				System.exit(1);
+				
+			}
+
+			@Override
+			public void buttonReleased(Button b) {
+				
+			}});
+		
 		ArrayList<indi> pok1 = new ArrayList<>();
 		pok1.add(new indi(1.7, 3.8, 0.003, 30, 1));
 		pok1.add(new indi(1.6, 3.9, 0.003, 35, 1));
-//		 new indi(1.3, 3.6, 0.004, 28, 1)
-//	  	 new indi(1.4, 4.0, 0.001, 31, 1)
-//	  	 new indi(1.8, 3.7, 0.002, 40, 1)
-//	  	 new indi(1.5, 3.5, 0.005, 29, 1)
-//	  	 new indi(1.2, 3.5, 0.003, 32, 1)
-//	  	 new indi(1.1, 3.1, 0.006, 45, 1)
-//	  	 new indi(1.3, 3.6, 0.004, 28, 1)
-//	  	 new indi(1.6, 3.9, 0.003, 34, 1)
-		
+		pok1.add(new indi(1.3, 3.6, 0.004, 28, 1));
+		pok1.add(new indi(1.4, 4.0, 0.001, 31, 1));
+		pok1.add(new indi(1.8, 3.7, 0.002, 40, 1));
+		pok1.add(new indi(1.5, 3.5, 0.005, 29, 1));
+		pok1.add(new indi(1.2, 3.5, 0.003, 32, 1));
+		pok1.add(new indi(1.1, 3.1, 0.006, 45, 1));
+		pok1.add(new indi(1.3, 3.6, 0.004, 28, 1));
+		pok1.add(new indi(1.6, 3.9, 0.003, 34, 1));
 		Button.waitForAnyPress();
-		experiment e = new experiment(pok1);
+		experiment e = new experiment(pok1, "test");
 		e.run();
 	}
 
-}
+} 
